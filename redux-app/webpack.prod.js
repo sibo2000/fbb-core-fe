5bbeee987e565718e53af8ca2b31d0044661c8f9
stevep124 <stephen.perry83@gmail.com>
2017-11-22 09:37:44 +0000

    modified webpack config

--- redux-app/webpack.prod.js
+++ redux-app/webpack.prod.js
@@ -1,25 +1,28 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const path = require('path');
+var buildPath = path.resolve(__dirname, 'public','build');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
  module.exports = merge(common, {
+  devtool: 'source-map',
   output: {
-    path: 'build',
+    path: buildPath,
     filename: '[name].bundle.js',
   },
-      plugins: [
-        new UglifyJSPlugin({
-          sourceMap: true,
-          compress: true
-        }),
-        new ExtractTextPlugin({
-          filename: "./style/[name].css",
-          allChunks: true
-        }),
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify('prod')
-        })
-      ]
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true,
+      compress: true
+    }),
+    new ExtractTextPlugin({
+      filename: "./style/[name].css",
+      allChunks: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('prod')
+    })
+  ]
  });
